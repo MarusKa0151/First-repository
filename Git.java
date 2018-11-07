3113d83cf6780aa73e4a2ebfc65e01b72009c8eb
Mája <m.skola>
2018-11-07 19:06:56 +0100

    Základy jak pracovat s gitem.

---
+++ Git.java
@@ -0,0 +1,35 @@
+public class Git {
+    public static void main(String[] args) {
+		//pamatuje si historii
+		
+		//repozitář (repository) = souhrnný název pro verzovaný adresář/soubory
+		
+		//commit = označení jedné změny, pojmenovaná záplata
+		
+		//fork = kopie jiného repozitáře (zjednodušeně)
+		
+		//git config --global user.name "Vaše Jméno"
+		
+		//git config --global user.email "váš e-mail"
+		
+		//git příkaz --help 
+		
+		//git clone cesta-k-repozitari = když pokračujeme v programu
+		
+		//verzování = sledování změn, git init = pro úplně nový program
+		
+		//git status = vypíše, které soubory byly změněny a které byly přidány (a dosud nejsou verzované)
+		
+		//git log = zobrazení historie, nahoře ten nejnovější
+		
+		//git diff = zobrazení současných změn jako záplaty
+		
+		//git add soubor = přidání souboru, nebo změny, přidá to do příštího commitu
+		
+		//git commit -m "Popis změny"
+		
+		//git push = odeslání změn na server
+		
+		//git pull = přijetí změn ze vzdáleného serveru
+	}
+}
